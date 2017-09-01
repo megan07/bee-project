3ac803ffe751116855dbfd2efc101aa948d2215c
megan07 <meganhallberg@gmail.com>
2017-09-01 17:50:42 -0400

    add the results page, some comments, and some optimization - add in route from geocode

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ from flask import request
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -18,10 +19,14 @@ def search_addresses():
     address = {
                 "original": request.form["my_address"],
                 "state": [],
-                "zip": []
+                "zip": [],
               }
 
     # addresses are typically typed in a similar order
+    # there could potentially be more string parsing here
+    # for street name etc, but i think it's common for people
+    # to search by city, state at the very least
+    # state should filter it enough, but there could definitely be more
     pieces = request.form["my_address"].split()
     if len(pieces) > 1:
         # the zip code is typically last
@@ -29,24 +34,30 @@ def search_addresses():
             address["zip"] = [pieces[-1]]
 
         # or they may have skipped the zip and just ended with a state
-        elif re.match("\w{2}", pieces[-1]):
+        elif pieces[-1] in search.STATES:
             address["state"] = [pieces[-1]]
 
-    if "state" not in address or "zip" not in address:
-        # if state or zip weren't provided before, we'll see if we can find it with geocode
-        address_options = geocode.call_api( address=request.form["my_address"] )
-        for geo_addr in address_options:
-            for t in gedo_addr["address_components"]:
-                if "administrative_area_level_1" in t["types"]:
-                    address["state"].append( t["types"]["short_name"] )
-                elif "postal_code" in t["types"]:
-                    address["zip"].append( t["types"]["short_name"] )
+    # see how much we can narrow down with the geo code
+    address_options = geocode.call_api( address=address["original"] )
+    for geo_addr in address_options:
+        state = ""
+        zip_code = ""
 
-    print (address, "\n\n")
+        for t in geo_addr["address_components"]:
+            if "postal_code" in t["types"]:
+                address["zip"].append( t["short_name"] )
+                zip_code = t["short_name"]
+            
+            if "administrative_area_level_1" in t["types"]:
+                address["state"].append( t["short_name"] )
+                state = t["short_name"]
 
-    print (search.filter_locations(address))
+            # this should correct any address errors, hopefully
+            if "route" in t["types"]:
+                address["geo"] = " ".join( [t["long_name"], state, zip_code] )
 
-    return request.form["my_address"]
+    # print out a template that lists the addresses that matched
+    return render_template("found.html", addresses=search.filter_locations(address))
 
 
 if __name__ == '__main__':
--- snippets/search.py
+++ snippets/search.py
@@ -1,28 +1,53 @@
 from fuzzywuzzy import process
 
+# read the us addresses file right away,
+# so that we don't have to keep opening it and closing it
+fp = open("data/us-addresses.tsv")
+
+headers = fp.readline().strip().split("\t")
+AVAILABLE_ADDRESSES = []
+
+# we'll make a set of states we can use for parsing the
+# address in the application
+STATES = set()
+
+for line in fp.readlines():
+    pieces = line.strip().split("\t")
+    loc = dict( zip(headers, pieces) )
+    loc["orig"] = " ".join(pieces)
+
+    AVAILABLE_ADDRESSES.append(loc)
+    STATES.add(loc["state_abbrev"])
+
+# filter locations will go through our available locations
+# from the file we read in.  it will then filter out which
+# locations the search is likely looking for based on state
+# and zip code.  we could potentially narrow down even more
+# but for now this helps a lot
 def filter_locations(search_term):
-    fp = open("data/us-addresses.tsv")
-
-    headers = fp.readline().strip().split("\t")
-
     locations = []
-    for line in fp.readlines():
-
-        loc = dict( zip(headers, line.strip().split("\t")) )
+    for addr in AVAILABLE_ADDRESSES:
 
         # try and narrow down the locations some
         search_state = search_term.get("state", None)
         search_zip = search_term.get("zip", None)
 
-        if len(search_state) > 0 or len(search_zip) > 0:
-            if loc["zip"] in search_zip:
-                locations.append(loc)
-            elif loc["state_abbrev"] in search_state:
-                locations.append(loc)
+        if len(search_zip) > 0:
+            if addr["zip"] in search_zip:
+                locations.append(addr["orig"])
+        elif len(search_state) > 0:       
+            if addr["state_abbrev"] in search_state:
+                locations.append(addr["orig"])
         else:
-            locations.append(loc)
+            locations.append(addr["orig"])
 
     fp.close()
 
-    print ("Locations length: ", len(locations))
-    print (process.extract(search_term["original"], locations))
+    # if the geo address we got earlier has enough information, lets use that one
+    if search_term.get("geo", "") >= search_term["original"]:
+        filtered =  process.extract(search_term["geo"], locations)
+    else:
+        filtered =  process.extract(search_term["original"], locations)
+
+    # only return options that are more likely matches (80 or better)
+    return ([options[0] for options in filtered if options[1] > 80])
